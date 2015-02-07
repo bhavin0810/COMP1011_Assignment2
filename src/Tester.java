7919a1b50f00019882292082b215743a665def18
bhavin0810 <bhavin0810@gmail.com>
2015-02-06 20:42:02 -0500

    Create the object of the SuperHero class and called the method of the SuperHero Class and Hero Class

--- src/Tester.java
+++ src/Tester.java
@@ -3,11 +3,18 @@
  * @author: Bhavin Patel
  * @Description: This Class Tester to test the program
  * @version 1.0  Feb-06-2015 Creating Tester Class with main method 
+ * @version 2.0  Feb-06-2015 Create the object of the SuperHero class and
+ *                    called the method of the SuperHero Class and Hero Class
  */
 
 public class Tester {
 
 	public static void main(String[] args) {
 
+		//Create a object of the SupoeHero Class		
+		SuperHero bhavin = new SuperHero("Bhavin");
+		bhavin.showPowers();
+		bhavin.fight();
+				
 	}
 }
