bcfc34b8a5b49eed27092a2a40d6c1bb2fc9fa31
bhavin0810 <bhavin0810@gmail.com>
2015-02-06 20:31:20 -0500

    Creating the SuperHero Class with necessary Properties and constructor

---
+++ src/SuperHero.java
@@ -0,0 +1,20 @@
+/**
+ * @author: Bhavin Patel
+ * @Description: Implementation Of the SuperHero Class with necessary properties and methods
+ * @version 1.0  Jan-30-2015 Creating the SuperHero Class with necessary Properties and constructor
+ * 
+ *  */
+
+public class SuperHero extends Hero {
+
+	String[] superpowers = new String[3] ;
+	
+	/**
+     * Constructor to create objects of class SuperHero
+     * @param name The name of the SuperHero
+     */
+	public SuperHero(String name) {
+		super(name);		
+	}
+	
+}
